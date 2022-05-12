2e9d0e503ae3449ea27f5a8936e0709e948e99a9
danger-master-dev <aspirin003@list.ru>
2022-05-12 11:38:19 +0300

    Initial commit

---
+++ Source/TestPrj/TestPrj.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestPrj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestPrj, "TestPrj" );
---
+++ Source/TestPrj/TestPrjGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestPrjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTPRJ_API ATestPrjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
